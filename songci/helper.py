709daf12494e31c3bbc7ab96b4980df8f2766437
Alexander Terbeznik <alexander.terbeznik@gmail.com>
2019-11-12 21:32:05 +0100

    remove read and write function
    ... solved in cli

--- songci/helper.py
+++ songci/helper.py
@@ -1,20 +1,5 @@
 import datetime
-import json
-import os
-
-from . import api
 
 
 def get_timestamp():
     return datetime.datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
-
-
-def read(path):
-    with open(path, "r") as f:
-        proxies = f.read().splitlines()
-    return proxies
-
-
-def write(result):
-    with open(f"{result['name']}.json", "w") as f:
-        json.dump(result, f, indent=4)
